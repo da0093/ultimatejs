8f8976e7e9ad2c71eb0352416f3151b5b7f4e1d5
Daniel Allendes <allendesdaniel93@gmail.com>
2024-01-21 20:32:21 -0300

    Comentarios y forma de escribirlos

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,22 @@
+// comentario
+
+/*
+todo lo que escibamos entremedio 
+de los slash y
+los asteriscos va a ser ignorado
+
+comentarios oficiales
+
+*/
+
+/**
+ * 
+ * Este es un comentario de 
+ * muchas
+ * lineas
+ * manera no oficial de comentarios
+ * 
+ */
+let numero= 42; // Este es otro comentario
+
+console.log(numero);
