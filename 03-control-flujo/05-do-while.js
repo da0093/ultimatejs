587a18cc877b07ee50211472fee29fc5b1afdea8
Daniel Allendes <allendesdaniel93@gmail.com>
2024-01-22 17:59:00 -0300

    Ejercicios de control de flujo

---
+++ 03-control-flujo/05-do-while.js
@@ -0,0 +1,16 @@
+//Cuales son los numeros pares
+
+let i = 0;
+while (i < 10) {
+    if(i % 2 == 0){
+        console.log('Numero par',i);
+    }
+    i++;
+}
+
+do{
+    if(i % 2 == 0){
+        console.log('Numero par',i);
+    }
+    i++;
+} while (1 < 2)
