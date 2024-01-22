6508e1b76c6d19b10537a4cb47a07dbb5ddca2c5
Daniel Allendes <allendesdaniel93@gmail.com>
2024-01-21 23:51:36 -0300

    Comparacion de operadores

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,12 @@
+//operadores relacionales
+let a = 10;
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+//operadores de igualdad
+console.log(a == 10);
+console.log(a != 10);
+console.log(a === '10');
+console.log(a !== '10');
