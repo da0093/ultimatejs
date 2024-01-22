baf49345b4192ae2155e0241b140d8f8193cf761
Daniel Allendes <allendesdaniel93@gmail.com>
2024-01-21 21:24:37 -0300

    operadores aritmeticos como son y como usarlos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'adicion'); //adicion
+// console.log(a - b, 'resta'); //resta
+// console.log(a * b), 'multiplicacion'; //multiplicacion
+// console.log(a / b, 'division'); //division
+// console.log(a % b, 'modulo'); //modulo
+// console.log(a ** b, 'potencia'); //potencia
+
+//incrementar
+console.log(++a); //incrementa el valor de a en 1+ siendo 1+5=6
+console.log(a++); //incrementa el valor de a en 1+ siendo 5+1=6
+console.log(a);
+//decrementar
+console.log(--a); 
+console.log(a--); 
+console.log(a);
