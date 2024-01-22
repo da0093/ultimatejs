f9ee744cdaf8505a9ea4a73f2030b292f571d273
Daniel Allendes <allendesdaniel93@gmail.com>
2024-01-21 23:58:19 -0300

    Operadores logicos

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,21 @@
+// Tenemos 3 operadores logicos
+// AND, OR , NOT
+
+//AND &&
+
+
+let mayor = false;
+let suscrito= true;
+
+//console.log (true && true);
+//console.log (false && true);
+console.log('operador and', mayor && suscrito);
+
+
+//OR ||
+console.log('operador or', mayor || suscrito);
+
+
+// NOT !
+console.log('operador not', !mayor);
+let catalogoInfantil = !mayor;
